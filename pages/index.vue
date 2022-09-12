27e37345f6721447df39b9e7c0cad84bb0253ea4
TheDavidGame <vlad.billI@bk.ru>
2022-09-12 19:35:04 +0300

    footer

--- pages/index.vue
+++ pages/index.vue
@@ -151,6 +151,54 @@
         </div>
       </div>
     </div>
+    <footer class="footer">
+      <div class="footer__wrapper">
+        <div class="footer__title">
+          <div class="footer__titleMain">
+            <p>To Get More Information</p>
+          </div>
+          <div class="footer__titleUnderMain">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie
+              in cursus eget interdum sed arcu nec, donec nibh.
+            </p>
+          </div>
+        </div>
+        <nav class="footer__nav">
+          <ul class="footer__list">
+            <li class="footer__listItem">
+              <img
+                src="../static/gmail.svg"
+                alt="logoUnderMain__secondPartPic"
+                class="footer__pic"
+              />
+              <p class="footer__text">Gmail</p>
+              <a href="#!" class="footer__link">vlados.billi@gmail.com</a>
+            </li>
+            <li class="footer__listItem">
+              <img
+                src="../static/github.svg"
+                alt="logoUnderMain__secondPartPic"
+                class="footer__pic"
+              />
+              <p class="footer__text">GitHub</p>
+              <a href="https://github.com/TheDavidGame" class="footer__link"
+                >TheDavidGame</a
+              >
+            </li>
+            <li class="footer__listItem">
+              <img
+                src="../static/telegram.svg"
+                alt="logoUnderMain__secondPartPic"
+                class="footer__pic"
+              />
+              <p class="footer__text">Telegram</p>
+              <a href="#!" class="footer__link">@TheDavidGame</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -190,6 +238,7 @@ export default {
 .wrapper {
   margin: 0 auto;
   max-width: 1400px;
+  padding-bottom: 80px;
 }
 .header {
   padding-bottom: 48px;
@@ -381,5 +430,64 @@ a {
   font-weight: 400;
   font-size: 18px;
   line-height: 21px;
+}
+.footer__wrapper {
+  width: 100%;
+  height: 590px;
+  background: linear-gradient(93.76deg, #c8e7d0 3.23%, #c6c0a6 98.45%);
+  background-size: cover;
+}
+.footer__title {
+  width: 600px;
+  margin: 0 auto;
+  padding-top: 76px;
+}
+.footer__titleMain {
+  font-weight: 500;
+  font-size: 54px;
+  line-height: 63px;
+}
+.footer__titleUnderMain {
+  width: 560px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+  text-align: center;
+}
+.footer__nav {
+  width: 1200px;
+  margin: 0 auto;
+  padding-top: 90px;
+}
+.footer__list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  list-style-type: none;
+}
+.footer__pic {
+  display: flex;
+  flex-wrap: wrap;
+  height: 70px;
+  width: 70px;
+  margin-bottom: 16px;
+}
+.footer__text {
+  width: 290px;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+  padding-bottom: 16px;
+}
+.footer__link {
+  width: 290px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 21px;
+}
+.footer__link:hover,
+.footer__link:focus,
+.footer__link:active {
+  opacity: 0.75;
 }
 </style>
